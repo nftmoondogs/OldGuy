1890d1ce2ab2c7dd9e8a595afead2a5a63ca7eab
nftmoondogs <nftmoondogs@gmail.com>
2024-06-16 21:24:17 +0530

    newcommit

--- components/content/text.js
+++ components/content/text.js
@@ -14,7 +14,7 @@ export const ExploreAI = () => {
         <div>
             <img src="https://i.ibb.co/wgXQCtN/pump.gif" width="300" height="100" alt="Description of the image"/>
             <p>$OldGuy Token is launched via pump.fun Meta as it's currently the best way to ensure equal distribution</p>
-            <a href='https://pump.fun/DC3pyv2erojqxuXs88fGCohGQxRuL6EGs5S8Wsp2pump'><FiExternalLink style={{marginTop:"10px"}} size={25}/></a>
+            <a href='https://pump.fun'><FiExternalLink style={{marginTop:"10px"}} size={25}/></a>
         </div>
     
     )
